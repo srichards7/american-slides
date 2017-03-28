fe99bb0bbc9f2d7aba066b9446cc0cf28d22b251
Chris Lawrence <chris@lordsutch.com>
2017-03-28 00:09:31 -0400

    Add emoji to narration toggle.

--- narration.js
+++ narration.js
@@ -51,7 +51,7 @@ function disableAutoplay(ob) {
     if(!ob)
         return;
 
-    ob.innerHTML = 'Enable Narration';
+    ob.innerHTML = '🔊 Enable Narration';
 }
 
 function enableAutoplay(ob) {
@@ -68,7 +68,7 @@ function enableAutoplay(ob) {
     if(!ob)
         return;
     
-    ob.innerHTML = 'Disable Narration';
+    ob.innerHTML = '🔇 Disable Narration';
 }
 
 function autoplayOn() {
